1b26ba3e3ad935bb7958fd77f87f597844ab81c2
Tushar <rocktush20@gmail.com>
2023-03-31 03:28:41 +0530

    social-media

--- server/routers/auth.js
+++
@@ -1,8 +0,0 @@
-const middleAuth = require("../middlewares/authmiddle");
-const {loginController, signupController} = require("../controllers/auth");
-const router = require("express").Router();
-
-router.post("/login",middleAuth,loginController);
-router.post("/signup",middleAuth,signupController);
-
-module.exports = router;
